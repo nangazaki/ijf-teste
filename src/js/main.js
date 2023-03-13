eb3f5b0084138b30991fbd0df82a16c23c14fe37
Helder Cambuta <heldercambuta44@gmail.com>
2023-03-13 11:45:04 +0100

    fix: Actualização das funcionalidades.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,16 @@
 
 const links = document.querySelectorAll('.link-dropdown')
 const dropdowns = document.querySelectorAll('.dropdown')
+const search = document.querySelector('.search')
+const inputSearch = document.querySelector('.input')
 
 links.forEach((link, i) => {
   link.addEventListener('click', () => {
     console.log('Clicou')
-    dropdowns[i].classList.toggle('active')
+    dropdowns[i].classList.toggle('dropdown-enter')
   })
+})
+
+search.addEventListener('click', () => {
+  inputSearch.classList.toggle('dropdown-enter')
 })
